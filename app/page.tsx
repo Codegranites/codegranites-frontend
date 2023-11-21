1737b7f96033a269854ad2cf9ae3d4112b3b40ba
Victory Odumeh <vodumeh@gmail.com>
2023-11-21 16:30:29 +0100

    implemented the contact us page

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,17 @@ import Hero from './Components/home/Hero';
 import WhyUs from './Components/home/WhyUs';
 import HowItWorks from './Components/about/OurValues';
 import Clients from './Components/home/Clients';
-import Form from './Components/contacts/Form';
-import ContactUs from './Components/contacts/ContactUs';
 
-// import ContactHero from './Components/contactus/ContactHero'
 
 export default function Home() {
   return (
     <>
-      <ContactUs />
-      {/* <Hero />
+      <Hero />
       <main className="max-container w-full flex flex-col">
         <WhyUs />
         <HowItWorks />
         <Clients />
-      </main> */}
+      </main>
     </>
   );
 }
